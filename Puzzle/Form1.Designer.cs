8d2a72e59ba977851da84d7081e82ed966ab187a
unknown <morscor21@gmail.com>
2020-11-27 20:23:20 +0100

    all at once (Still needs timer and win popup)
    Signed-off-by: unknown <morscor21@gmail.com>

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.buttonsContainer = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonsContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 111);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(209, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 111);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(387, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 111);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(28, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 111);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(209, 175);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 111);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(387, 175);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 111);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(28, 318);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 111);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(209, 318);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 111);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(387, 318);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 111);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button9_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Welcome To Puzzle The Best Puzzle Game Ever";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(114, 618);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(227, 41);
+            this.startBtn.TabIndex = 10;
+            this.startBtn.Text = "FullFill The Puzzle";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // buttonsContainer
+            // 
+            this.buttonsContainer.Controls.Add(this.button9);
+            this.buttonsContainer.Controls.Add(this.button8);
+            this.buttonsContainer.Controls.Add(this.button7);
+            this.buttonsContainer.Controls.Add(this.button6);
+            this.buttonsContainer.Controls.Add(this.button5);
+            this.buttonsContainer.Controls.Add(this.button4);
+            this.buttonsContainer.Controls.Add(this.button3);
+            this.buttonsContainer.Controls.Add(this.button2);
+            this.buttonsContainer.Controls.Add(this.button1);
+            this.buttonsContainer.Location = new System.Drawing.Point(114, 117);
+            this.buttonsContainer.Name = "buttonsContainer";
+            this.buttonsContainer.Size = new System.Drawing.Size(558, 463);
+            this.buttonsContainer.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(426, 633);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "00:00";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(536, 618);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(136, 40);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "State";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 633);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Timer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 846);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonsContainer);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.buttonsContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Panel buttonsContainer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+
+        System.Timers.Timer Timer;
+        int minutes, seconds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Shuffle()
+        {
+            int i, j, Rn;
+            int[] a = new int[9];
+            Boolean flag = false;
+            i = 1;
+            do
+            {
+                Random rnd = new Random();
+                Rn = ((rnd.Next(0, 8)) + 1);
+                for (j = 1; j <= i; j++)
+                {
+                    if (a[j] == Rn)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    a[i] = Rn;
+                    i = i + 1;
+                }
+            }
+            while (i <= 8);
+            button1.Text = Convert.ToString(a[1]);
+            button2.Text = Convert.ToString(a[2]);
+            button3.Text = Convert.ToString(a[3]);
+            button4.Text = Convert.ToString(a[4]);
+            button5.Text = Convert.ToString(a[5]);
+            button6.Text = Convert.ToString(a[6]);
+            button7.Text = Convert.ToString(a[7]);
+            button8.Text = Convert.ToString(a[8]);
+            button9.Text = "";
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        private void button9_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //Timer = new System.Timers.Timer();
+            //Timer.Interval = 1000; // more like 1s
+            //Timer.Elapsed += OnTimeEvent;
+        }
+    }
+}
